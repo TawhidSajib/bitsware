ecd98b918c6480b79663d088fc8708b9f4916993
imation82 <18244941+imation82@users.noreply.github.com>
2022-12-11 02:38:40 +0600

    Header updated!

--- components/Header.vue
+++ components/Header.vue
@@ -1,15 +1,15 @@
 <template>
     <section class="header position-relative">
-            <img src="~/assets/svg/header-ornament-1.svg" alt="Header Ornament" class="position-absolute header__top--1">
-            <img src="~/assets/svg/header-ornament-1.svg" alt="Header Ornament" class="position-absolute header__bottom--1">
-            <img src="~/assets/svg/header-ornament-2.svg" alt="Header Ornament" class="position-absolute header__bottom--2">
+            <img :src="header_ornament_1" alt="Header Ornament" class="position-absolute header__top--1">
+            <img :src="header_ornament_1" alt="Header Ornament" class="position-absolute header__bottom--1">
+            <img :src="header_ornament_2" alt="Header Ornament" class="position-absolute header__bottom--2">
         <div class="container">
             <div class="row">
                 <div class="col-lg-7">
                     <h2>Build your</h2>
                     <h1>
                         <span class="position-relative">
-                            <img src="~/assets/svg/header-font-style.svg" alt="Header Font" class="position-absolute start-0 bottom-0">
+                            <img :src="header_font_style" alt="Header Font" class="position-absolute start-0 bottom-0">
                             Offshorey
                         </span> Team
                     </h1>
@@ -17,12 +17,12 @@
                     <p>Bitsware is a leading design agency based in USA. We help startups &amp; Fortune 30+ companies delight humans on the other side of the screen.</p>
                     <NuxtLink to="/" class="btn-blue">
                         <div class="d-flex align-items-center">
-                            View Project<img src="~/assets/svg/arrow-circle-right.svg" alt="">
+                            View Project<img :src="right_arrow" alt="Arrow">
                         </div>
                     </NuxtLink>
                 </div>
                 <div class="col-lg-5 d-flex">
-                    <img src="~/assets/svg/header-banner.svg" alt="Header Banner" class="d-block mx-auto img-fluid">
+                    <img :src="header_banner" alt="Header Banner" class="d-block mx-auto img-fluid">
                 </div>
             </div>
         </div>
@@ -30,7 +30,23 @@
 </template>
 
 <script>
+
+import headerOrnament1 from '~/assets/svg/header-ornament-1.svg';
+import headerOrnament2 from '~/assets/svg/header-ornament-2.svg';
+import headerFontStyle from '~/assets/svg/header-font-style.svg';
+import headerBanner from '~/assets/svg/header-banner.svg';
+import rightArrow from '~/assets/svg/arrow-circle-right.svg'
+
 export default {
-    name: 'Header'
+    name: 'Header',
+    data() {
+        return {
+            header_ornament_1: headerOrnament1,
+            header_ornament_2: headerOrnament2,
+            header_font_style: headerFontStyle,
+            header_banner: headerBanner,
+            right_arrow: rightArrow
+        }
+    }
 }
 </script>
